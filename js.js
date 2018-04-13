3b5cd581caae3e2f6d2c6d5ab78840c338721cef
Eve Jones <evejones@reimageds-MacBook-Pro.local>
2018-04-12 18:42:17 -0700

    A good start

---
+++ js.js
@@ -0,0 +1,40 @@
+var userFilters = ["dog", "cat", "small animal", "bird", "lizard"];
+
+// A function to create checkboxes for the user to select which type of pet(s) they're looking for
+
+// A click event to use ajax to return ten pets that meet the criteria
+$("#btn").on("click", function(){
+    var possiblePet = $(this).attr("data-pet");
+    var apiKey = "key=9e688b61b50439d4ab91fb4d3031fa6c&limit=10";
+    var queryUrl = "http://api.petfinder.com/schemas/0.9/petfinder.xsd?alt=json" + possiblePet + apiKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        dataType: "xml",
+        success: function(xml){
+            $(xml).find("dog").each(function(){
+             $("#output").append($(this).attr("code") + "<br />");
+            });
+        }
+    })
+    .then(function(response) {
+        console.log("Img: ", response);
+        var results = response.data;
+        console.log(results);
+
+        //Loops through every result
+        for (var i = 0; i < results.length; i++) {
+
+        var petDiv = $("<div>");
+        petDiv.addClass("box");
+        var imageURL = $("<img>");
+        
+        imageURL.attr(response[i].image);
+        petDiv.append(imageURL);
+
+        console.log (queryURL);
+        };
+
+    });
+}) 
