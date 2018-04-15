b53193fc2f20fe68198290f9f49d26b861361311
Eve Jones <evejones@reimageds-MacBook-Pro.local>
2018-04-15 11:05:21 -0700

    Photos and basic info loading to the DOM

--- js.js
+++ js.js
@@ -20,21 +20,37 @@ $.ajax({
         output: 'basic',
         format: 'json',
         count: 10,
-        location: userLocation
+        location: userLocation,
     },
     success:function(response) {
         console.log(response);
         console.log("Is this working?");
         var results = response.petfinder.pets.pet;
-        //Loops through every result
+
+        //Loops through every result to add photos to the page
         for (var i = 0; i < results.length; i++) {
 
             var petDiv = $("<div>");
             petDiv.addClass("box");
             var imageURL = $("<img>");
+            var name = results[i].name.$t;
+            var age = results[i].age.$t;
+            var email = results[i].contact.email.$t;
+            var phone = results[i].contact.phone.$t;
+            console.log(name, age);
+            var p = $("<p>");
+            p.text("Name: " + name);
+            var p2 = $("<p>");
+            p2.text("Age: " + age);
+            var p3 = $("<p>");
+            p3.text("Contact info: " + email + ", " + phone);
             
             imageURL.attr("src", results[i].media.photos.photo[2].$t);
+            
             petDiv.append(imageURL);
+            petDiv.append(p);
+            petDiv.append(p2);
+            petDiv.append(p3);
             $("#output").prepend(petDiv);
 
             console.log (results[i]);
@@ -58,4 +74,3 @@ $( "#button" ).click(function() {
 //"https://api.rescuegroups.org/http/"
 //9e688b61b50439d4ab91fb4d3031fa6c
 //
-        
